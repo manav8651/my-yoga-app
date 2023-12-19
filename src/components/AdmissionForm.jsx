0e8df6deafc56ad6c91a028ad2ef78c9c4417641
Manav Raj <mr.manav.raj.2805@gmail.com>
2023-12-19 23:11:13 +0530

    Added front

--- src/components/AdmissionForm.jsx
+++ src/components/AdmissionForm.jsx
@@ -16,7 +16,7 @@ const AdmissionForm = () => {
     if (age >= 18 && age <= 65) {
       try {
         // Make a POST request to the backend API
-        const response = await fetch('http://localhost:5000/api/users', {
+        const response = await fetch('https://my-yoga-app-back.onrender.com/api/users', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
